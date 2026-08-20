5c30acd28b5aa2df4e0675b46af78fb69fd1f74f
dhanush <dhanushprasana2003.07.13@gmail.com>
2026-08-20 21:51:35 +0530

    removed unneccessary file

--- test_node.js
+++
@@ -1,8 +0,0 @@
-
-console.log("Starting...");
-try {
-  const inc = require("./src/modules/seed-data/data/crimie/crime_incident.json");
-  console.log("Incidents loaded:", inc.length);
-} catch (e) {
-  console.error(e);
-}
